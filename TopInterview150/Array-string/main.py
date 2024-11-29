1cce35c4f6436630f09781d956e458d148266e3c
jmolenaa <jmolenaa@student.codam.nl>
2024-11-29 10:51:53 +0100

    added script to make directories and files

---
+++ TopInterview150/Array-string/main.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+
+
+def test(nums, expected):
+
+
+if __name__ == '__main__':
+    test([1, 2, 3], [1, 2, 3])
+    test([2, 2, 3], [2, 3])
+    test([0, 0, 1, 1, 1, 2, 2, 3, 3, 4], [0, 1, 2, 3, 4])
---
+++ TopInterview150/Array-string/mkexercise.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir $1
+touch $1/problem
+cp main.py $1/main.py
